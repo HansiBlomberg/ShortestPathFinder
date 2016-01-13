25d2cb9acc01e0c0c58cb7ad58b38c06cc841ae4
Hansi Blomberg <g-i-t-h-a-n-s-i@netcare.se>
2016-01-13 17:26:50 +0100

    Added a class Node för graph nodes

---
+++ ShortestPathFinder/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeTransportationLimited.Graphs
+{
+    /// <summary>
+    /// This class holds a node object
+    /// 
+    /// Properties:
+    /// ID = The ID of the node object
+    /// firstNeighborID = The ID of the first neighbor node
+    /// secondNeighborID = The ID of the second neighbor node
+    /// </summary>
+    class Node
+    {
+        public int ID { get; set; }
+        public int firstNeighborID { get; set; }
+        public int secondNeighborID { get; set; }
+    }
+}
