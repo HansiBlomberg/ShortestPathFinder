387850f7f438fab57fd38eb52f85caaa05214316
Hansi Blomberg <g-i-t-h-a-n-s-i@netcare.se>
2016-01-14 15:43:27 +0100

    Added PreviousID and Distance as properties to the Node class and updated the constructor.

--- ShortestPathFinder/Node.cs
+++ ShortestPathFinder/Node.cs
@@ -20,28 +20,44 @@ namespace NodeTransportationLimited.Graphs
         /// ID of this Node
         /// </summary>
         public int ID { get; set; }
+
+
+        /// <summary>
+        /// ID of previous node on optimal path from source
+        /// </summary>
+        public int? PreviousID { get; set; }
+
+
         /// <summary>
         /// List of ID:s of neighbouring nodes
         /// </summary>
         public List<int> NeighborIDs { get; set; }
+       
         /// <summary>
         /// Visited flag to be used by get shortest path algorithm
         /// </summary>
-        public bool isVisited;
+        public bool IsVisited;
+
+        /// <summary>
+        /// Distance property, shortest found distance to start node
+        /// </summary>
+        public int? Distance { get; set; }
 
 
-       /// <summary>
-       /// Constructor - will create a new Node with one neighbour Node ID
-       /// 
-       /// </summary>
-       /// <param name="id"></param>
-       /// <param name="neighbourID"></param>
+        /// <summary>
+        /// Constructor - will create a new Node with one neighbour Node ID
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="neighbourID"></param>
         public Node(int id, int neighbourID)
         {
             this.ID = id;
             this.NeighborIDs = new List<int>();
             NeighborIDs.Add(neighbourID);
-            this.isVisited = false;
+            this.IsVisited = false;
+            this.Distance = null; // null is for infinity
+            this.PreviousID = null; // null until found
         }
 
     }
